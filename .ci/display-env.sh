2ddd057fc154ad77a1978e096ef165df17962e65
Andreas Dangel <andreas.dangel@adangel.org>
2020-11-11 20:12:15 +0100

    use curl

--- .ci/display-env.sh
+++ .ci/display-env.sh
@@ -42,21 +42,7 @@ COL_RED="\e[31m"
 COL_RESET="\e[0m"
 COL_YELLOW="\e[33;1m"
 
-function log_error() {
-    echo -e "${COL_RED}[ERROR  ] $*${COL_RESET}"
-}
-
-function log_info() {
-    echo -e "${COL_YELLOW}[INFO   ] $*${COL_RESET}"
-}
-
-function log_success() {
-    echo -e "${COL_GREEN}[SUCCESS] $*${COL_RESET}"
-}
-
-function log_debug() {
-    echo -e "[DEBUG  ] $*"
-}
+source $(dirname $0)/logger.inc
 
 log_error "Error logging color test"
 log_info "Info logging color test"
